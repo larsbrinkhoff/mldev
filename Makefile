eb7dad7da0be02e4ad8931f73108ec9eecf19c5b
Lars Brinkhoff <lars@nocrew.org>
2017-02-07 11:32:45 +0100

    Add reply codes.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: mldev mlslv
 
-mldev: src/mldev.o
+mldev: src/mldev.h src/mldev.o
 	$(CC) -o $@ $^
 
-mlslv: src/mlslv.o
+mlslv: src/mldev.h src/mlslv.o
 
 clean:
 	-rm -f src/*.o
--- src/mldev.h
+++ src/mldev.h
@@ -61,3 +61,78 @@
 
 #define CREUSE	14	/* Reinitialize yourself, because we are about to
 			   be reused */
+
+#define RDATA	1	/* Input data (word count includes data words and
+			   byte count word)
+			   arg1:	# of bytes of data
+			   args:	Data, packed ILDB-style into words.
+			   Note:  Slave must always send fully populated
+			   words except when EOF is reached. */
+
+#define ROPENI	2	/* Input OPEN status; 1 or 11 arguments
+			   arg1: -1 => succeeded >0 => OPEN loss number
+			   arg2-arg11 are present only if arg1 is -1
+			   arg2:	Real device name (usually SIXBIT/DSK/)
+			   arg3:	 Real FN1
+			   arg4:	Real FN2
+			   arg5:	Real SNAME
+			   arg6:	File length
+			   arg7:	Byte size open in
+			   arg8:	File length in byte size written in
+			   arg9:	Byte size written in
+			   arg10:	FILDTP - -1 if RFDATE wins on this file
+			   arg11:	Creation data (if RFDATE wins) */
+
+#define ROPENO	3	/* Output OPEN status.  1 or 11 arguments
+			   arg1:	-1 => succeeded >0 => OPEN loss number
+			   arg2-arg11 present only if arg1 is -1
+			   arg2:	Real device name
+			   arg3:	Real FN1
+			   arg4:	Real FN2
+			   arg5:	Real SNAME
+			   arg6:	File length (-1 => FILLEN fails on
+					this device)
+			   arg7:	Byte size open in
+			   arg8:	File length in byte size written in
+			   arg9:	Byte size written in
+			   arg10:	FILDTP - -1 if RFDATE wins on this file
+			   arg11:	Creation data (if RFDATE wins) */
+
+#define REOF	4	/* EOF on input
+			   Ignored argument */
+
+#define RFDELE	5	/* DELETE/RENAME status.
+			   1st argument:
+			   RH:	-1 => succeeded
+				0 => failure code
+			   LH:	-1 => DELETE or RENAME
+				0 => RENAME while open
+			   If RENAME while open call succeeded, there are
+			   4 more arguments:
+			   arg2:	Real device name
+			   arg3:	Real FN1 (as renamed)
+			   arg4:	Real FN2
+			   arg5:	Real SNAME */
+
+#define RNOOP	6	/* No-op reply
+			   arg: Echo argument send by no-op command */
+
+#define RACCES	7	/* Acknowledge access
+			   arg:	Transaction number from CACCES */
+
+#define RSYSCL	010	/* Respond to system call.  9 arguments
+			   arg1:	<error code or 0>,,<# of times call
+					skipped>
+			   arg2:	1st value from system call
+			   ...
+			   arg9:	8th value from system call */
+
+#define RICLOS	011	/* Respond to input CLOSE */
+
+#define ROCLOS	012	/* Respond to output CLOSE */
+
+#define RIOC	013	/* Reflect an IOC error
+			   arg:	IOC error code, right-justified in word */
+
+#define RREUSE	014	/* Acknowledge a CREUSE
+			   Ignored argument */
